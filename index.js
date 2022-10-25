e5890a3374cd5b09e7fcf6cdee24302b62a3d789
JustinRuiz321 <107062112+JustinRuiz321@users.noreply.github.com>
2022-10-25 13:18:56 -0400

    Pushing code to get askBCS help

--- index.js
+++ index.js
@@ -4,60 +4,70 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
     {
         type: 'input',
-        name: 'project title',
+        name: 'projectTitle',
         message: 'What is going to be the name of your Project?'
     },
     {
         type: 'input',
-        name: 'project description',
+        name: 'projectDescription',
         message: 'How would you describe your Project?'
     },
     {
         type: 'input',
-        name: 'project installs',
+        name: 'projectInstalls',
         message: 'Which packages would need to be installed for your Project?'
     },
     {
         type: 'input',
-        name: 'project instructions',
+        name: 'projectInstructions',
         message: 'How would you describe instructions on how to use your Project'
     },
     {
         type: 'list',
-        name: 'project license',
+        name: 'projectLicense',
         message: 'Of the following, please choose an open source license. If no answer is provided, Apache will be chosen.',
         choices: ['Apache License 2.0', 'BSD 3-Clause', 'BSD 2-Clause', 'GNU General Public License', 'MIT License', 'Mozilla Public License 2.0']
     },
     {
         type: 'input',
-        name: 'project guidlines',
+        name: 'projectGuidlines',
         message: 'If you would like to give another developer the oppurtunity to make any changes to your Project, please provide guidlines here:'
     },
     {
         type: 'input',
-        name: 'project testing',
+        name: 'projectTesting',
         message: 'How is your project being tested?'
     },
     {
         type: 'input',
-        name: 'developer email',
+        name: 'developerEmail',
         message: 'Please enter your email here'
     },
     {
         type: 'input',
-        name: 'github account',
+        name: 'githubAccount',
         message: 'Please enter your Github account username here'
     }
-];
+]);
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then((data) => {
+        data.projectLicense = data.projectLicense.toUpperCase();
+        fs.writeFileSync('README.md', generateMarkdown(data))
+    })
+    .then(() => console.log('Completed README.md file'))
+    .catch((err) => console.log(err));
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if(license === "APACHE 2.0") {
+        return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
+    } else if (license === "BSD 3-CLAUSE") {
+        return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
+    } else if(license === "BSD 2-CLAUSE") {
+        return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]";
+    } else if (license === "GNU GENERAL PUBLIC LICENSE") {
+        return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (license === "MIT LICENSE") {
+        return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (license === "MOZILLA PUBLIC LICENSE 2.0") {
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]";
+    } else {
+        return "N/A";
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+    if(license === "APACHE 2.0") {
+        return "(https://opensource.org/licenses/Apache-2.0)";
+    } else if (license === "BSD 3-CLAUSE") {
+        return "(https://opensource.org/licenses/BSD-3-Clause)";
+    } else if(license === "BSD 2-CLAUSE") {
+        return "(https://opensource.org/licenses/BSD-2-Clause)";
+    } else if (license === "GNU GENERAL PUBLIC LICENSE") {
+        return "(https://opensource.org/licenses/gpl-license)";
+    } else if (license === "MIT LICENSE") {
+        return "(https://opensource.org/licenses/MIT)";
+    } else if (license === "MOZILLA PUBLIC LICENSE 2.0") {
+        return "(https://opensource.org/licenses/MPL-2.0)";
+    } else {
+        return "N/A";
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
+  ## Project Description
+ ${data.projectDescription}
+
+ ## Project Installs
+ ${data.projectInstalls}
+
+ ## Project Instructions
+ ${data.projectInstructions}
+
+ ## Project License
+ ${renderLicenseBadge(data.projectLicense)}
+ ${renderLicenseLink(data.projectLicense)}
+
+ ## Project Guidlines
+ ${data.projectGuidlines}
+
+ ## Project Testing
+ ${data.projectTesting}
+
+ ## Email
+ ${data.developerEmail}
+
+ ## Github Account
+ ${data.githubAccount}
 `;
 }
 
